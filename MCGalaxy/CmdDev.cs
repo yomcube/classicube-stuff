8ee4682d3bb90c1f54ef1da5794db54fde75c20f
TomCube <121310847+yomcube@users.noreply.github.com>
2023-05-17 14:39:31 -0500

    Add plugin support

--- MCGalaxy/CmdDev.cs
+++ MCGalaxy/CmdDev.cs
@@ -9,14 +9,24 @@ public class CmdDev : Command
 	public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
 	public override void Use(Player p, string message)
 	{
-		if (message.Length == 0) { Help(p); return; }
-		Command.Find("Compile").Use(p, message);
+		Command compilecmd = Command.Find("Compile");
+		Command plugincmd  = Command.Find("Plugin");
+		if (message.Length == 0 || message.ToLower() == "plugin") { Help(p); return; }
+		if (message.ToLower().StartsWith("plugin")) {
+			string newmessage = message.Split("plugin ")[1];
+			compilecmd.Use(p, "plugin " + newmessage);
+			plugincmd.Use(p, "unload " + newmessage);
+			plugincmd.Use(p, "load " + newmessage);
+			return;
+		}
+		compilecmd.Use(p, message);
 		Command.Find("CmdUnload").Use(p, message);
 		Command.Find("CmdLoad").Use(p, message);
+		return;
 	}
 	public override void Help(Player p)
 	{
 		p.Message("&T/Dev");
-		p.Message("&HCompiles, unloads, and loads a command.");
+		p.Message("&HCompiles, unloads, and loads a command or plugin.");
 	}
 }
